842954ac754b2c22af4f03e602ba1c43bbfb2742
chethanakt3006@gmail.com <chethanakt3006@gmail.com>
2021-07-28 11:31:08 +0530

    first commit

---
+++ src/component/Hooks/useState.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./style.css";
+import  { useState } from 'react';
+
+const UseState = () => {
+  // const initialData = 15;
+  const [myNum, setMyNum] = useState(0);
+  // console.log(myNum);
+
+  return (
+    <>
+      <div className="center_div">
+        <p>{myNum}</p>
+        <div class="button2" onClick={() => setMyNum(myNum + 1)}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          INCR
+        </div>
+        <div
+          class="button2"
+          onClick={() => (myNum > 0 ? setMyNum(myNum - 1) : setMyNum(0))}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          DECR
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UseState;
